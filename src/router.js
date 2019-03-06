08404c8c424393acdb8912f76c7ef960c1fa7c31
Samuel Abächerli <samu_hugo@Samuels-MacBook-2.local>
2019-03-06 10:21:47 +0100

    1 - Initialize Vue 2 - Add Remote Git

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,16 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'Register',
+      meta: {
+        title: 'Tennis Court',
+        metaTags: [
+          {
+            name: 'Booking Engine',
+            content: 'Enable your tennis courts to be booked automatically.'
+          }
+        ]
+      },
+      component: () => import(/* webpackChunkName: "register" */ './views/Register.vue')
     }
   ]
 })
---
+++ src/views/Register.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="">
+    <h1>TENNIS COURT</h1>
+    <h2>A Booking Engine for your Tennis Courts</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style lang="css" scoped>
+h1 {
+  position: fixed;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 5em;
+  color: black;
+}
+h2 {
+  position: relative;
+  top: 280px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 1.9em;
+  color: darkgray;
+}
+</style>

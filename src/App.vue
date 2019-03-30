24de68328062f771bcd50cefaa377b2e095ff2f8
Samuel Abächerli <samu_hugo@Samuels-MacBook-2.local>
2019-03-30 23:19:11 +0100

    10 - "Enter" as alternative to clicking

--- src/App.vue
+++ src/App.vue
@@ -133,5 +133,6 @@ input:-webkit-autofill:hover,
 input:-webkit-autofill:focus,
 input:-webkit-autofill:active  {
     -webkit-box-shadow: 0 0 0 30px white inset !important;
+    outline: none;
 }
 </style>
